532966052b97664acda6330ac4af49349391026c
Sergey <poletvnebesa@yandex.ru>
2019-03-01 01:49:54 +0300

    done

--- Lesson1/ViewController.swift
+++ Lesson1/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginTextField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // жест нажатия
