c8d796ecb1d33d29327a865123bd7e6e8879041f
Sergey <poletvnebesa@yandex.ru>
2019-03-01 00:08:36 +0300

    Initial Commit

---
+++ Lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Sergey Mikhailov on 01/03/2019.
+//  Copyright © 2019 Sergey Mikhailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
